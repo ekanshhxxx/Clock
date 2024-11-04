22096e9d971215697bc1dced3790a2d39757579b
Ekansh Dhiman <144600707+ekanshhxxx@users.noreply.github.com>
2024-11-04 14:41:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import DigitalClock from './components/Clock';
+
+
+function App() {
+  return (
+    <div className="App">
+      <DigitalClock />
+    </div>
+  );
+}
+
+export default App;
